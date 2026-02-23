25ae66c9e443d2355230da02c0934a5cdf5d1eef
Aswin P T <aswinsureshpt@gmail.com>
2026-02-23 12:43:55 +0530

    Time: 206 ms (84%), Space: 52.1 MB (34.89%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -0,0 +1,18 @@
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        n = len(s)
+        
+        if n < k:
+            return False
+        
+        needed = 1 << k   
+        seen = set()
+        
+        for i in range(n - k + 1):
+            substring = s[i:i+k]
+            seen.add(substring)
+            
+            if len(seen) == needed:
+                return True
+        
+        return len(seen) == needed
