b4e6df08485c19b3733d493616c7ad16ed44788f
Aswin P T <aswinsureshpt@gmail.com>
2026-04-30 02:34:48 +0530

    Added solution - LeetHub

---
+++ 702730-sum-of-array-GFG/702730-sum-of-array.py
@@ -0,0 +1,12 @@
+#User function Template for python3
+class Solution:
+	def arraySum(self, arr):
+	    total = 0
+	    
+	    for n in arr:
+	        total = total + n
+	        
+	        
+	    return total 
+	   
+   		# code here
