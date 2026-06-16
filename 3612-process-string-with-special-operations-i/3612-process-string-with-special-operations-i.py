ed2fadd01a68d7747305824f7718e14163b1f5f7
Aswin P T <aswinsureshpt@gmail.com>
2026-06-17 01:26:47 +0530

    Time: 0 ms (100%), Space: 23.4 MB (70.59%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def processStr(self, s: str) -> str:
+        result = ""
+
+        for ch in s:
+            if ch.islower():
+                result += ch
+            elif ch == '*':
+                result = result[:-1]
+            elif ch == '#':
+                result += result
+            elif ch == '%':
+                result = result[::-1]
+
+        return result
+        
